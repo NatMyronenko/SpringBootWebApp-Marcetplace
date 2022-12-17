b7b1ec16eb5d4034e7cfbc3f75ecfa4a87858adc
rubusta <n.a.rubusta3@gmail.com>
2022-12-17 19:15:54 +0100

    run app

--- src/main/java/com/example/buysell/MarketplaceApplication.java
+++ src/main/java/com/example/buysell/MarketplaceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BuysellApplication {
+public class MarketplaceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BuysellApplication.class, args);
+		SpringApplication.run(MarketplaceApplication.class, args);
 	}
 
 }
