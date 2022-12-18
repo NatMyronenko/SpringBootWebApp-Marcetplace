b20515d3852a647ff6cc92c49ee26d9174fa11a9
rubusta <n.a.rubusta3@gmail.com>
2022-12-18 14:49:17 +0100

    changes in methods

--- src/main/java/com/example/marketplace/controllers/ProductController.java
+++ src/main/java/com/example/marketplace/controllers/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,8 +16,8 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/")
-    public String products(Model model) {
-        model.addAttribute("products", productService.listProducts());
+    public String products(@RequestParam(name = "title",required = false)String title, Model model) {
+        model.addAttribute("products", productService.listProducts(title));
         return "products";
     }
     @GetMapping("/product/{id}")
@@ -27,7 +28,7 @@ public class ProductController {
     @PostMapping("/product/create")
     public String createProduct(Product product){
         productService.saveProduct(product);
-                return "redirect:/";
+        return "redirect:/";
     }
     @PostMapping("/product/delete/{id}")
     public String deleteProduct(@PathVariable Long id){
