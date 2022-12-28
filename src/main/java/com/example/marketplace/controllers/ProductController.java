77bc322ee5ca00bf3ee857abdb5909fb20d62959
rubusta <n.a.rubusta3@gmail.com>
2022-12-28 09:58:07 +0100

    added ios, methods, updated

--- src/main/java/com/example/marketplace/controllers/ProductController.java
+++ src/main/java/com/example/marketplace/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.marketplace.controllers;
 
 import com.example.marketplace.models.Product;
+import com.example.marketplace.models.User;
 import com.example.marketplace.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -20,17 +21,20 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/")
-    public String products(@RequestParam(name = "title", required = false) String title,Principal principal, Model model) {
+    public String products(@RequestParam(name = "title", required = false) String title, Principal principal, Model model) {
         model.addAttribute("products", productService.listProducts(title));
-        model.addAttribute("user",productService.getUserByPrincipal(principal));
+        model.addAttribute("user", productService.getUserByPrincipal(principal));
+        model.addAttribute("searchWord", title);
         return "products";
     }
 
     @GetMapping("/product/{id}")
-    public String productInfo(@PathVariable Long id, Model model) {
+    public String productInfo(@PathVariable Long id, Model model, Principal principal) {
         Product product = productService.getProductById(id);
+        model.addAttribute("user", productService.getUserByPrincipal(principal));
         model.addAttribute("product", product);
         model.addAttribute("images", product.getImages());
+        model.addAttribute("authorProduct", product.getUser());
         return "product-info";
     }
 
@@ -42,8 +46,16 @@ public class ProductController {
     }
 
     @PostMapping("/product/delete/{id}")
-    public String deleteProduct(@PathVariable Long id) {
-        productService.deleteProduct(id);
-        return "redirect:/";
+    public String deleteProduct(@PathVariable Long id, Principal principal) {
+        productService.deleteProduct(productService.getUserByPrincipal(principal) ,id);
+        return "redirect:/my/products";
+    }
+
+    @GetMapping("/my/products")
+    public String userProducts(Principal principal, Model model) {
+        User user = productService.getUserByPrincipal(principal);
+        model.addAttribute("user", user);
+        model.addAttribute("products", user.getProducts());
+        return "my-products";
     }
 }
