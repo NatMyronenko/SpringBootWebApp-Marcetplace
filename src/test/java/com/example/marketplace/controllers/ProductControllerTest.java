47b8ba83199590ef3dee77e2832a6689d106205f
rubusta <n.a.rubusta3@gmail.com>
2022-12-30 12:02:07 +0100

    product controller tests

--- src/test/java/com/example/marketplace/controllers/ProductControllerTest.java
+++ src/test/java/com/example/marketplace/controllers/ProductControllerTest.java
@@ -4,19 +4,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void productsTest() throws Exception{
-        mockMvc.perform(get(""))
-                an
-
-
-    }
+ //   @Test
+//    void productsTest() throws Exception{
+//        mockMvc.perform(get("/"))
+//                .andExpect(status().isOk())
+//                .andExpect(view().name("products"))
+//                .andExpect(view().name("user"))
+//                .andExpect(view().name("searchWord"));
+//}
 
     @Test
     void productInfo() {
